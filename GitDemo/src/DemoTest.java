f564b3ae9aec45a6842ba45016e6e9c98ee20994
narasimhulu.sagi <narasimhulu.sagi@SAGIN1-TL1.thcg.net>
2018-04-24 12:02:02 +0530

    1st commit Change-Id: Ibfd2b4f7c60264e2a434f45428eac39e250cbd78 Signed-off-by: narasimhulu.sagi <narasimhulu.sagi@SAGIN1-TL1.thcg.net>

---
+++ GitDemo/src/DemoTest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author narasimhulu.sagi
+ *
+ */
+public class DemoTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+
+		System.out.println("First Commit");
+	}
+
+}
